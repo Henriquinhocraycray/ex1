04fb29afda6d97ae5cd6a5d3511f8bdaf0aa757a
Henriquinhocraycray <130668229+Henriquinhocraycray@users.noreply.github.com>
2023-09-29 15:21:33 -0300

    commit do gostosao

---
+++ ex7.php
@@ -0,0 +1,14 @@
+<?php
+
+    $cont = 4;
+
+    while ($cont <= 30) {
+        if ($cont == 24) {
+            break;
+        }
+
+        echo "Contador : $cont <br>";
+
+        $cont += 2;
+    }
+?>
